1a9db5b6900fc156300285bd72a01a193476a25c
Sowrya-11 <76764710+Sowrya-11@users.noreply.github.com>
2021-04-20 13:19:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+para1= document.getElementById("para_1")
+para2= document.getElementById("para_2")
+input1 = document.getElementById("input_1")
+input1 = document.getElementById("2input_1")
+
+//mam it is correct but not working
+function sub1(){
+    para1.join("input_1")
+    document.getElementById("blank").innerHTML=para1;
+    
+}
+function sub2(){
+    para1.join("input_1")
+    document.getElementById("blank").innerHTML=para2;
+    
+}
